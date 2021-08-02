7d0fe6a1c92acd136e23b67079befe3c628b5678
Isaak Antunez <isaakantunez73@gmail.com>
2021-08-02 01:02:07 -0600

    Version-4

--- App.js
+++ App.js
@@ -4,14 +4,17 @@ import {Provider as PaperProvider} from "react-native-paper";
 import theme from './src/themes';
 import Navigation from './src/components/navigation';
 import {Provider as AuthProvider} from "./src/providers/AuthContext";
+import {Provider as NoteProvider} from "./src/providers/NoteContext"
 
 export default function App() {
   return (
-    <AuthProvider>
+    <NoteProvider>
+      <AuthProvider>
       <PaperProvider theme={theme}>
        <Navigation/>
       </PaperProvider>
     </AuthProvider>
+    </NoteProvider>
   );
 }
 
